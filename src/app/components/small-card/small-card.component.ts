3079a6aaf673caa78ba2820c103c09046280d570
Gabriel Silva <gabriel127.gk@gmail.com>
2024-01-02 15:23:59 -0300

    changes: small-card - ok - 2

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.scss']
+  styleUrls: ['./small-card.component.scss', './small-card.component.responsive.scss']
 })
 export class SmallCardComponent {
   @Input() photoCover: string = '';
