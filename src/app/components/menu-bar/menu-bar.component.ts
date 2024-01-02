ff3f55af02ab08321808461e0fd1b5475519408c
Gabriel Silva <gabriel127.gk@gmail.com>
2024-01-02 17:23:43 -0300

    changes: refatorando scss - 2

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.scss']
+  styleUrls: ['./menu-bar.component.scss', './menu-bar.component.responsive.scss']
 })
 export class MenuBarComponent {
 
